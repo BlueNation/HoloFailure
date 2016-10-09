83d3b387572628e2d2d738649c91273e9f93eeea
DIYNG-DISEASE\Bass <basdxz>
2016-10-09 21:44:27 +0100

    dunk

--- holofailure.lua
+++ holofailure.lua
@@ -2,16 +2,16 @@
 --Layer 1 {OK, Fault, Crit} Layer 2 {A,B,C} Layer 3 {Red 0-255, Green 0-255, Blue 0-255,}
 
 local colours = {--HSV 30deg shifr iirc
-	{{  0,  0, 64},{  0,  0,128},{  0,  0,255}},
-	{{  0, 16, 64},{  0, 32,128},{  0, 64,255}}, 
-	{{  0, 32, 64},{  0, 64,128},{  0,128,255}}} 
+    {{  0,  0, 64},{  0,  0,128},{  0,  0,255}},
+    {{  0, 16, 64},{  0, 32,128},{  0, 64,255}}, 
+    {{  0, 32, 64},{  0, 64,128},{  0,128,255}}} 
 
 function EZCONCAT(tab)
     local txt=""
     for i=1,48 do
         for j=1,32 do
             for k=1,48 do
-				txt=txt..tab[i][j][k]
+                txt=txt..tab[i][j][k]
     end end end
     return txt
 end
@@ -29,10 +29,10 @@ mFrame.__index=function() return "\0" end--dont init the mFrame again... using t
 
 for i = 1,48 do
     tFrame[i]={}
-	for j = 1,32 do
-		tFrame[i][j]={}
-		setmetatable(tFrame[i][j],mFrame)
-	end
+    for j = 1,32 do
+        tFrame[i][j]={}
+        setmetatable(tFrame[i][j],mFrame)
+    end
 end
 
 
