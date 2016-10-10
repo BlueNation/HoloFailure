63b782414ac6d94ff8a2e5c68953bb297c963efe
Technus <daniel112092@gmail.com>
2016-10-10 13:07:38 +0200

    mmk

--- holofailure.lua
+++ holofailure.lua
@@ -1,5 +1,11 @@
---Variables and functions
---Layer 1 {OK, Fault, Crit} Layer 2 {hex,hex,hex}
+--imports
+local unicode = require('unicode')
+local event = require('event')
+local term = require('term')
+local fs = require('filesystem')
+local component = require("component")
+local keyboard =  require("keyboard")
+
 local meta={}--main meta table holding obj definitions (meta tables and meta methods)
 do meta.machineArray={}     meta.machineArray.typeName="machineArray"
     function meta.machineArray:new()
@@ -65,21 +71,24 @@ do meta.machine={}          meta.machine.typeName="machine"
     end
 end
 do meta.frame={}            meta.frame.typeName="frame"
-    function meta.frame:new(mDefault)
+    function meta.frame:new(mDefault)--default should be "\0" - "\3"
         local o={
             ["default"]=mDefault}
         setmetatable(o,self)
         self.__index=self
-        self["voxels"]={}--to do later code that handles checking machine state
+        self["voxels"]={}
+        self["pallete"]={}
         
-        local tDefaulter={}
-        tDefaulter.__index=function() return mDefault end
-        setmetatable(self.voxels,tDefaulter)
+        local tDefault={}--seting default value metatable
+        tDefault.__index=function() return mDefault end
+        setmetatable(self.voxels,tDefault)
+        
+        setmetatable(self.pallete,const.tDefaultPallete)--no custom default palletes
         return o
     end 
     
-    function meta.frame:set(x,y,z,mState)
-        local index=x+y*48+z*48*32
+    function meta.frame:set(x,y,z,mState)--states are normal numbers
+        local index=func.xyzToPosition(x,y,z)
         if mState~=self.default then
             self.voxels[index]=tostring(mState)
             return true
@@ -92,17 +101,40 @@ do meta.frame={}            meta.frame.typeName="frame"
         end
     end
     
-    function meta.frame:render(cHolo)
+    function meta.frame:setPalette(tPallete)
+        return self.pallete=func.iDup(tPallete)
+    end
+    
+    function meta.frame:sendPallete(cHolo)
+        return cHolo.setPalleteColor(1,self.pallete[1]),
+               cHolo.setPalleteColor(2,self.pallete[2]),
+               cHolo.setPalleteColor(3,self.pallete[3])
+    end
+    
+    function meta.frame:sendVoxels(cHolo)
+        return cHolo.setRaw(self:toString())
+    end
+    
+    function meta.frame:toString()
         local str=""
-        for i=1,48*32*48 do
+        for i=0,const.resolutionMaxSize do
             str=str..self.voxels[i]
         end
-        cHolo.setRaw(str)
         return str
+    end
+    
+    function meta.frame:setFromString(str)
+        self.voxels=setmetatable({},getmetatable(self.voxels))--if table has metatable must be constructed like so, it forwards metatable back to the object/table
+        for i=1,#str do
+            if str:sub(i,i)~=self.default then
+                self.voxels[i-1]=str:sub(i,i)
+            end
+        end
+        return self.voxels
     end
 end
 
-local func={}
+local func={}--functions
 do --table test/duplication
     function func.isTable(tab)
         return type(tab)=="table"
@@ -155,51 +187,72 @@ do --instance of functions
     end
 end
 do --translations of coordinates
+    --yes our coord system starts from 0 !!! so if u make a iteration over pixels do:
+    --for i=0,const.resolutionMaxSize do
     function func.xyzToPosition(x,y,z)
-        return x+48*y+1536*z -- y is shifted by xMax+1, z is shifted by (xMax+1)*(yMax+1)
-                             --yes our coord system starts from 0 !!!
+        return x+48*y+1536*z -- y is shifted by xMax, z is shifted by (xMax)*(yMax)
     end
     
-    function func.positionToXYZ(pos)
-        local Z=math.floor(pos/1536)--48*32 full multiples of 1536 OK
-        local X=math.fmod(pos,48)--0-47 OK
+    function func.positionToXYZ(pos)--i am avoiding use of that
+        local Z=math.floor(pos/1536)-- full multiples of 1536 OK
+        local X=math.fmod(pos,48)-- 0-47 modulus gets the beggining OK
         return X,math.floor((pos-Z-X)/48),Z --calculate Y as the remaining thing
     end
 end
---INIT END
+do --other util funtions (single functions without groups)
+    function func.cyclicRandom(actual,minimum,maximum)--a random function for generating integer in range but different from previous one
+        if minimum>maximum then minimum,maximum=maximum,minimum end
+        local range=maximum-minimum
+        local randomNumber=actual+math.random(1,range-1)
+        if randomNumber>maximum then randomNumber=randomNumber-range end
+        return randomNumber
+    end
+end
+
+local const={}--Constants, NEVER UNDER ANY CIRCUMSTANCES WRITE NEW VALUES TO THINGS DEFINED HERE!!! (if u need to, move them out of that table)
+do --resolution constants
+    const.resolutionMax={48,32,48}-- x,y,z max size
+    
+    const.resolutionMaxSize=73728 -- 48*48*32
+    
+    const.tDefaultPallete={}
+    const.tDefaultPallete.__index=const.tDefaultPallete
+    const.tDefaultPallete[1]=0x0000ff--R
+    const.tDefaultPallete[2]=0x00ff00--G
+    const.tDefaultPallete[3]=0xff0000--B
+end
+
+--VARIABLES
+local holo = component.hologram
 
 local colours = {--HSV 30deg shifr iirc
     {0x000040,0x000080,0x0000ff},
     {0x0075a2,0x00cfc1,0x2af5ff}, 
     {0xe9ce2c,0xef8a17,0xc84c09}} 
-
---Load Basics
-
-local component = require("component")
-local keyboard =  require("keyboard")
-local holo = component.hologram
-holo.clear()
-holo.setScale(.3)
-
+    
 local rPallet = 1
 
+--Objects
 local machineArray1=meta.machineArray:new()
 local frame1=meta.frame:new("\0")
 
-while true do
+--do once
 
-    rPallet=rPallet+math.random(1,#colours-1)--"cyclic range" randomness with the same object exclusion
-    if rPallet>#colours then rPallet=rPallet-#colours end
+holo.clear()
+holo.setScale(.3)
 
-    for i=1,3 do
-        holo.setPaletteColor(i, colours[rPallet][i])
-    end
+
+while true do--main loop
+    rPallet=func.cyclicRandom(rPallet,1,3)
+    
+    frame1:setPalette(colours[rPallet])
+    frame1:sendPallete(holo)
 
     --Define 3D frame
     local newmachine1=meta.machine:new(math.random(0,48),math.random(0,32),math.random(0,48),math.random(1,3))--creating new machine
     machineArray1:add(newmachine1)--adding to machine array
     machineArray1:update(frame1)--updating frame content
-    frame1:render(holo)--
+    frame1:sendVoxels(holo)--
     
     os.sleep(5)
 end
