1303c55db2efe490bf6f1aa8bba220e0efa585d3
Technus <daniel112092@gmail.com>
2016-10-10 18:14:26 +0200

    no message

--- holofailure.lua
+++ holofailure.lua
@@ -18,23 +18,19 @@ do meta.machineArray={}     meta.machineArray.typeName="machineArray"
     
     function meta.machineArray:add(mMachine)
         for k,v in ipairs(self.machines) do
-            if      v.posX==mMachine.posX and --can align if similar
-                    v.posY==mMachine.posY and --double indent when doing long conditionals plz
-                    v.posZ==mMachine.posZ then
+            if v.pos==mMachine.pos then
                 return false
             end
         end
-        self.machines[#self.machines+1]=mMachine--yeah put the machine there
+        table.insert(self.machines,mMachine)--yeah put the machine there
         return true
     end
     
     function meta.machineArray:del(mMachine)
         for i=#self.machines,1,-1 do
-            if      self.machines[i].posX==mMachine.posX and --can align if similar
-                    self.machines[i].posY==mMachine.posY and --double indent when doing long conditionals plz
-                    self.machines[i].posZ==mMachine.posZ then
+            if self.machines[i].pos==mMachine.pos then
                 --table.remove(self.machines,i)
-                self.machines[i]=-999
+                self.machines[i].state=const.removeCode
                 return true
             end
         end
@@ -43,27 +39,28 @@ do meta.machineArray={}     meta.machineArray.typeName="machineArray"
     
     function meta.machineArray:update(mFrame)
         for i=#self.machines,1,-1 do
-            if self.machines[i]==-999 then
-                mFrame:set(self.machines[i].posX,self.machines[i].posY,
-                           self.machines[i].posZ,self.mFrame.default)
+            if self.machines[i].state==const.removeCode then
+                mFrame:set(self.machines[i].position,mFrame.default)
                 table.remove(self.machines,i)
             else
-                mFrame:set(self.machines[i].posX,self.machines[i].posY,
-                           self.machines[i].posZ,self.machines[i].state)
+                mFrame:set(self.machines[i].position,self.machines[i].state)
             end
         end
     end
 end  
 do meta.machine={}          meta.machine.typeName="machine"
-    function meta.machine:new(x,y,z,state)
+    function meta.machine:new(position,state)
         local o={--using o like u know object
-            ["posX"]=x,
-            ["posY"]=y,
-            ["posZ"]=z,
+            ["pos"]=position,
             ["state"]=state}
         setmetatable(o,self)
         self.__index=self
+        --self["check"]={}
         return o
+    end
+    
+    function meta.machine:newFromXYZ(x,y,z,state)
+        return this:new(func.xyzToPosition(x,y,z),state)
     end
     
     function meta.machine:set(mState)
@@ -71,26 +68,25 @@ do meta.machine={}          meta.machine.typeName="machine"
     end
 end
 do meta.frame={}            meta.frame.typeName="frame"
-    function meta.frame:new(mDefault)--default should be "\0" - "\3"
+    function meta.frame:new(mDefault)--default should be 0 - 3
         local o={
-            ["default"]=mDefault}
+            ["default"]=mDefault or 0}
         setmetatable(o,self)
         self.__index=self
         self["voxels"]={}
         self["pallete"]={}
         
         local tDefault={}--seting default value metatable
-        tDefault.__index=function() return mDefault end
+        tDefault.__index=function() return self.default end
         setmetatable(self.voxels,tDefault)
         
         setmetatable(self.pallete,const.tDefaultPallete)--no custom default palletes
         return o
     end 
     
-    function meta.frame:set(x,y,z,mState)--states are normal numbers
-        local index=func.xyzToPosition(x,y,z)
+    function meta.frame:setPosition(index,mState)--states are normal numbers
         if mState~=self.default then
-            self.voxels[index]=tostring(mState)
+            self.voxels[index]=mState
             return true
         else
             if self.voxels[index] then
@@ -99,6 +95,10 @@ do meta.frame={}            meta.frame.typeName="frame"
             end
             return false 
         end
+    end
+    
+    function meta.frame:set(x,y,z,mState)--states are normal numbers
+        return this:setPosition(func.xyzToPosition(x,y,z),mState)
     end
     
     function meta.frame:setPalette(tPallete)
@@ -116,19 +116,60 @@ do meta.frame={}            meta.frame.typeName="frame"
     end
     
     function meta.frame:toString()
-        local str=""
-        for i=0,const.resolutionMaxSize do
-            str=str..self.voxels[i]
+        local str
+        local char=string.char
+        for i=1,const.resolutionMaxSize do
+            str=str..char(self.voxels[i])
         end
         return str
     end
     
     function meta.frame:setFromString(str)
-        self.voxels=setmetatable({},getmetatable(self.voxels))--if table has metatable must be constructed like so, it forwards metatable back to the object/table
+        self.voxels=func.tClean(self.voxels)--if table has metatable must be constructed like so, it forwards metatable to the object/table
         for i=1,#str do
-            if str:sub(i,i)~=self.default then
-                self.voxels[i-1]=str:sub(i,i)
+            if str:byte(i)~=self.default then ---48 changes string byte to number if it is a single digit 
+                self.voxels[i]=str:byte(i)
             end
+        end
+        return self.voxels
+    end
+    
+    function meta.frame:toCompact()
+        local char=string.char
+        local str=char(self.default)..
+                  func.formatColor(self.pallete[1])..
+                  func.formatColor(self.pallete[2])..
+                  func.formatColor(self.pallete[3])
+        for i=1,const.resolutionMaxSize,4 do
+            str=str..char(
+                self.voxels[i]*64+
+                self.voxels[i+1]*16+
+                self.voxels[i+2]*4+
+                self.voxels[i+3])
+        end
+        return str
+    end
+    
+    function meta.frame:setFromCompact(str)
+        self.default=str:byte(1)
+        local tDefault={}--seting default value metatable
+        tDefault.__index=function() return self.default end
+        setmetatable(self.voxels,tDefault)
+        
+        self.pallete[1]=func.readColor(str,2)
+        self.pallete[2]=func.readColor(str,5)
+        self.pallete[3]=func.readColor(str,8)
+        
+        self.voxels=func.tClean(self.voxels)
+        local band=bit32.band
+        local rshift=bit32.rshift
+        local insert=table.insert
+        local b
+        for i=11,#str do
+            insert(self.voxels,rshift(band(colorNumber,0xC0),6))
+            insert(self.voxels,rshift(band(colorNumber,0x30),4))
+            insert(self.voxels,rshift(band(colorNumber,0x0C),2))
+            insert(self.voxels,band(colorNumber,0x03))
         end
         return self.voxels
     end
@@ -156,19 +197,23 @@ do --table test/duplication
         return t
     end
     
-    function func.oDup(tab)--used to duplicate objects
-        local t=setmetatable({},getmetatable(tab))--reuse metatables
-        for k,v in pairs(tab) do
+    function func.oDup(object)--used to duplicate objects
+        local obj=setmetatable({},getmetatable(object))--reuse metatables
+        for k,v in pairs(object) do
             if func.isTable(v) then
-                t[k]=func.oDup(v)--function is dumb will halt the process on looped table
+                obj[k]=func.oDup(v)--function is dumb will halt the process on looped table
             else
-                t[k]=v
+                obj[k]=v
             end
         end
-    return t
+        return obj--reference to new table
+    end
+    
+    function func.tClean(tab)--cleans the table keeping metatable
+        return setmetatable({},getmetatable(tab))--cheeky scrubness, result shuld be put into the variable
     end
 end
-do --instance of functions 
+do --instanceof and obj helper functions 
     function func.instanceOf(tab,mTab)--checks if the object (tab) is instance of the thing
         return getmetatable(tab)==mTab
         --func.instanceOf(someMachine,meta.machine) should give true
@@ -187,16 +232,18 @@ do --instance of functions
     end
 end
 do --translations of coordinates
-    --yes our coord system starts from 0 !!! so if u make a iteration over pixels do:
-    --for i=0,const.resolutionMaxSize do
+    --x ,y ,z starts from 0
+    --positions start from 1
+
     function func.xyzToPosition(x,y,z)
-        return x+48*y+1536*z -- y is shifted by xMax, z is shifted by (xMax)*(yMax)
+        return 1+x+48*y+1536*z -- y is shifted by xMax, z is shifted by (xMax)*(yMax)
     end
     
-    function func.positionToXYZ(pos)--i am avoiding use of that
+    function func.positionToXYZ(pos)
+        pos=pos-1
         local Z=math.floor(pos/1536)-- full multiples of 1536 OK
         local X=math.fmod(pos,48)-- 0-47 modulus gets the beggining OK
-        return X,math.floor((pos-Z-X)/48),Z --calculate Y as the remaining thing
+        return X,math.floor((pos-Z*1536)/48),Z --calculate Y as the remaining thing
     end
 end
 do --other util funtions (single functions without groups)
@@ -208,18 +255,37 @@ do --other util funtions (single functions without groups)
         return randomNumber
     end
 end
+do --color formating
+    function func.formatColor(colorNumber)--no alpha
+        local band=bit32.band
+        local rshift=bit32.rshift
+        local char=string.char
+        local r=rshift(band(colorNumber,0xFF0000),16)
+        local g=rshift(band(colorNumber,0x00FF00),8)
+        local b=band(colorNumber,0x0000FF)
+        return char(r)..char(g)..char(b)
+    end
+    
+    function func.readColor(str,startPos)
+        return str:byte(startPos)*65536+
+               str:byte(startPos+1)*256+
+               str:byte(startPos+2)
+    end
+end
 
 local const={}--Constants, NEVER UNDER ANY CIRCUMSTANCES WRITE NEW VALUES TO THINGS DEFINED HERE!!! (if u need to, move them out of that table)
 do --resolution constants
+    const.removeCode=-999
+
     const.resolutionMax={48,32,48}-- x,y,z max size
     
     const.resolutionMaxSize=73728 -- 48*48*32
     
     const.tDefaultPallete={}
     const.tDefaultPallete.__index=const.tDefaultPallete
-    const.tDefaultPallete[1]=0x0000ff--R
+    const.tDefaultPallete[1]=0x00ffff--C - cos i hate red
     const.tDefaultPallete[2]=0x00ff00--G
-    const.tDefaultPallete[3]=0xff0000--B
+    const.tDefaultPallete[3]=0x0000ff--B
 end
 
 --VARIABLES
@@ -234,7 +300,7 @@ local rPallet = 1
 
 --Objects
 local machineArray1=meta.machineArray:new()
-local frame1=meta.frame:new("\0")
+local frame1=meta.frame:new(0)
 
 --do once
 
@@ -249,7 +315,7 @@ while true do--main loop
     frame1:sendPallete(holo)
 
     --Define 3D frame
-    local newmachine1=meta.machine:new(math.random(0,48),math.random(0,32),math.random(0,48),math.random(1,3))--creating new machine
+    local newmachine1=meta.machine:new(math.random(0,47),math.random(0,31),math.random(0,47),math.random(1,3))--creating new machine
     machineArray1:add(newmachine1)--adding to machine array
     machineArray1:update(frame1)--updating frame content
     frame1:sendVoxels(holo)--
